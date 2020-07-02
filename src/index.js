d9dada059897d1a85e52a7b7b2a61df21d922aba
fanye <1518878598@qq.com>
2020-07-02 16:06:34 +0800

    feat: test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from 'App'
+import HelloWorld from './components/HelloWorld'
 
 const componentLib = {
-    App
+    HelloWorld
 }
 
 Object.keys(componentLib).forEach(key=>{
