2f484564f680218e124fbd8cbc11523e204efc71
fanye <1518878598@qq.com>
2020-07-10 16:02:19 +0800

    feaT: 修改bug

--- src/lib/Cascade/index.js
+++ src/lib/Cascade/index.js
@@ -1,8 +1,12 @@
 import Cascade from './Cascade'
+import CacscadeItem from './CascadeItem'
 
 // 支持按需引用
 Cascade.install = function (Vue) {
     Vue.component(Cascade.name, Cascade);
+};
+CacscadeItem.install = function (Vue) {
+    Vue.component(CacscadeItem.name, CacscadeItem);
 };
 
 export default Cascade;
